a452808985b50e4cfb167f7f934a90938be7cd70
felixtimmel <f.timmel95@gmail.com>
2020-04-13 10:57:16 +0200

    landing page design for mobile

--- src/landing/LandingView.js
+++ src/landing/LandingView.js
@@ -1,24 +1,27 @@
-import React from 'react'
+import React from 'react';
 import './Landing.scss';
-import Button from '../buttons/ButtonView'
+import Logo from '../_assets/splash_screen/Logo.svg';
+import { Link } from "react-router-dom";
+
+require('./Landing.scss');
 
 const Landing = () => {
     return (
-        <div>
+        <div className="landing-container">
             <div className="content">
                 <div className="title">
-                    <h1>OVEO</h1>
+                    <img src={Logo} alt='Oveo logo'/>
                 </div>
                 <div className="description">
                     <h2>Découvrez une toute nouvelle façon d'apprécier votre musique</h2>
                 </div>
             </div>
             <div className="landing-btn">
-                <div>
-                    <Button content="s'inscire" link='/login'/>
+                <div className="signup-btn">
+                    <Link to='/signup'>S'INSCRIRE</Link>
                 </div>
-                <div>
-                    <Button content="se connecter" link='/signup'/>
+                <div className="login-btn">
+                    <Link to='/login'>SE CONNECTER</Link>
                 </div>
             </div>
         </div>
