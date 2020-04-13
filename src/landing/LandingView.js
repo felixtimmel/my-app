af1024700dd5626bc4ed4b872fa46d0767d4740f
felixtimmel <f.timmel95@gmail.com>
2020-04-13 11:19:42 +0200

    design with media queries

--- src/landing/LandingView.js
+++ src/landing/LandingView.js
@@ -8,21 +8,21 @@ require('./Landing.scss');
 const Landing = () => {
     return (
         <div className="landing-container">
-            <div className="content">
-                <div className="title">
-                    <img src={Logo} alt='Oveo logo'/>
-                </div>
-                <div className="description">
-                    <h2>Découvrez une toute nouvelle façon d'apprécier votre musique</h2>
-                </div>
+            <div className="title">
+                <img src={Logo} alt='Oveo logo'/>
             </div>
+
+            <div className="description">
+                <h2>Découvrez une toute nouvelle façon d'apprécier votre musique</h2>
+            </div>
+
             <div className="landing-btn">
-                <div className="signup-btn">
-                    <Link to='/signup'>S'INSCRIRE</Link>
-                </div>
-                <div className="login-btn">
-                    <Link to='/login'>SE CONNECTER</Link>
-                </div>
+                <Link to='/signup'>
+                    <div className="signup-btn">S'INSCRIRE</div>
+                </Link>
+                <Link to='/login'>
+                    <div className="login-btn">SE CONNECTER</div>
+                </Link>
             </div>
         </div>
     );
