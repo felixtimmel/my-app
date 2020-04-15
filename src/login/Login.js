f953013d6ae15d3387615c3c4456b4a2783afc39
felixtimmel <f.timmel95@gmail.com>
2020-04-15 14:33:29 +0200

    login page designed

--- src/login/Login.js
+++ src/login/Login.js
@@ -5,22 +5,34 @@ class Login extends React.Component {
     constructor(props) {
         super(props);
         this.ui = this.props.firebaseClass.getFirebaseUi();
+
+        this.state={
+            isPassVisible: false,
+        }
     }
+
+    onShowPassword = (field) => {
+        const { isPassVisible } = this.state;
+        this.setState({
+          isPassVisible: field.name === 'password' ? !isPassVisible : isPassVisible,
+        })
+      }
+
     componentDidMount() {
-      const { firebase } = this.props.firebaseClass;
+      /* const { firebase } = this.props.firebaseClass;
         this.ui.start('#firebaseui-auth-container', {
             signInOptions: [
                 firebase.auth.EmailAuthProvider.PROVIDER_ID
             ],
             // Other config options...
-        });
+        }); */
     }
     render() {
         return (
-        <>
-            <LoginView />
+        <div className="login-container">
+            <LoginView isPassVisible={this.state.isPassVisible}/>
             {/* {this.ui && <div id='firebaseui-auth-container'></div> } */}
-        </>
+        </div>
         )
     }
 }
--- src/login/LoginView.js
+++ src/login/LoginView.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import Logo from '../_assets/Inscription 1/Logo.svg'
+import fblogo from '../_assets/Inscription_2/facebook-2.svg'
+import googlelogo from '../_assets/Inscription_2/google-icon.svg'
+import eyeLogo from '../_assets/Page_connexion/grey_eye.svg';
+
+require('./Login.scss');
+require('../signup/signup.scss');
 
 export const LoginView = (props) => {
-    return (
-        <>
-            <div id='firebaseui-auth-container'></div>
-        </>
-    );
+	return (
+		<div className="signIn">
+			<div>
+				<img src={Logo} alt='Oveo logo' className="signIn__logo"/>
+			</div>
+			<h2>
+				Bienvenue !<br/>
+				Connecter vous à votre compte
+			</h2>
+			<div className="signIn__form">
+				<input placeholder="Email" type="text"/>
+				<div className="input-container">
+					<input placeholder="Mot de passe" type="text"/>
+					<span className="password_logo" /* onClick={() => props.onShowPassword(field)} */>
+						<img src={eyeLogo} alt='password logo' />
+					</span>
+				</div>
+				<p>Mot de passe oublié ?</p>
+			</div>
+			<button type='submit' className="signIn__submit_btn">Se connecter</button>
+			<div className="signIn__separator">
+				<h5>Ou connectez-vous avec</h5>
+			</div>
+			<div className="signIn__social">
+				<div className='signIn__social-btn'>
+					<button /* onClick={() => onSignUpWithFacebook()} */>
+							<img src={fblogo} alt="google logo"/>
+					</button>
+      		<p>Facebook</p>
+   			</div>
+				<div className='signIn__social-btn'>
+					<button /* onClick={() => onSignUpWithGoogle()} */>
+							<img src={googlelogo} alt="google logo"/>
+					</button>
+					<p>Google</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default LoginView;
