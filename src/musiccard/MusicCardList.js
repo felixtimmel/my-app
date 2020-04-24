2af0887f7c80754f3ebd1b78b4f62e057f6dcd2a
felixtimmel <f.timmel95@gmail.com>
2020-04-24 17:19:23 +0200

    put suggestion down the searchbar

--- src/musiccard/MusicCardList.js
+++ src/musiccard/MusicCardList.js
@@ -5,6 +5,7 @@ import {Redirect} from 'react-router-dom';
 const MusicCardList = ({ lastSongs, isSearching, searchTracks, topTracks, getMusicInfo, musicInfo }) => {
 	if (isSearching) {
 		return (
+			<>
 			<div className="vertical-music-card_list">
 				{searchTracks && searchTracks.length
 				? searchTracks.map((item) => 
@@ -14,6 +15,26 @@ const MusicCardList = ({ lastSongs, isSearching, searchTracks, topTracks, getMus
 				)
 				: null}
 			</div>
+			<div className="horizontal-music-card_list">               
+				{lastSongs && lastSongs.length
+				? lastSongs.map((item) => 
+					<div className="horizontal-music_card" 
+							onClick={() => getMusicInfo(item) }>
+						<MusicCard  name={item.name} artist={item.artist} image={item.url} key={item.id}/>
+					</div>  
+					)
+				: null}
+			</div>
+			<div className="vertical-music-card_list">
+					{topTracks && topTracks.length
+					? topTracks.map((item) => 
+						<div className="vertical-music_card" onClick={() => getMusicInfo(item)}>
+							<MusicCard  name={item.name} artist={item.artists[0].name} image={item.album.images[0].url} key={item.id}/>
+						</div>        
+					)
+					: null}
+			</div>
+			</>
 		) 
 	}
 	return (
